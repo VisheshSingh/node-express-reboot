94eed807bb832971a061fa70f0e133401ab66e4f
Vishesh Singh <VisheshSingh16@gmail.com>
2023-03-30 19:40:48 -0400

    connect to DB first then run server

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -23,9 +23,9 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB();
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
-      connectDB();
     });
   } catch (error) {
     console.log(error);
