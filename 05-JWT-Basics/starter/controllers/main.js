8f4aacbb37dbd18ce7b2093b6468b22a71a5f3a8
Vishesh Singh <VisheshSingh16@gmail.com>
2023-03-30 18:21:14 -0400

    sign & validate

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
@@ -6,15 +7,29 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomAPIError('Please provide valid username and password', 400);
   }
+  const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
+  });
 
-  res.status(200).json({ msg: 'Login success' });
+  res.status(200).json({ msg: 'Login success', token });
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100 + 1);
-  res
-    .status(200)
-    .json({ msg: 'Hello!', secret: `Your lucky number is ${luckyNumber}` });
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith(`Bearer `)) {
+    throw new CustomAPIError('No token!', 401);
+  }
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const luckyNumber = Math.floor(Math.random() * 100 + 1);
+    res.status(200).json({
+      msg: `Hello! ${decoded.username}!`,
+      secret: `Your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError('Forbidden!', 403);
+  }
 };
 
 module.exports = {
