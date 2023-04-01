8088ba199538d12cd7d898c384867e8d809d94cd
Vishesh Singh <VisheshSingh16@gmail.com>
2023-03-31 20:44:15 -0400

    duplicate instance error

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,15 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+    return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  if (err.code === 11000) {
+    return res
+      .status(StatusCodes.CONFLICT)
+      .json({ msg: 'User already exists!' });
+  }
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+};
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
