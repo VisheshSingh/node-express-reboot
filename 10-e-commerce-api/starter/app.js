5a95ee5fa01f3c8af108a99a8cb59969577bad3f
Vishesh Singh <VisheshSingh16@gmail.com>
2023-04-17 16:59:14 -0400

    route + errorhandler setup

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,11 +1,24 @@
+// EXPRESS
 const express = require('express');
+// ENV VARIABLES
 require('dotenv').config();
+// DB CONNECTION
 const connectDB = require('./db/connect');
+
+// ROUTES
 const productRoutes = require('./routes/productRoutes');
 
-const app = express();
+const { NotFoundError } = require('./errors');
+const errorHandler = require('./middleware/error-handler');
 
+const app = express();
+// MIDDLEWARES
+app.use(express.json());
+
+app.use('/api/v1', (req, res) => res.send('E-Commerce API'));
 app.use('/api/v1/products', productRoutes);
+app.use(NotFoundError);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
