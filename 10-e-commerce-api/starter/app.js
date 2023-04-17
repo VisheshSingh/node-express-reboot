39bf515626bf230b046d90a5c5893ceb4cbb2f4a
Vishesh Singh <VisheshSingh16@gmail.com>
2023-04-17 16:36:41 -0400

    setup express + connect DB

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,17 @@
-console.log('E-Commerce API');
+const express = require('express');
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+async function start() {
+  try {
+    await connectDB();
+    app.listen(PORT, console.log(`Server listening to PORT: ${PORT}`));
+  } catch (error) {
+    console.log('Server connection failed');
+  }
+}
+
+start();
--- 10-e-commerce-api/starter/db/connect.js
+++ 10-e-commerce-api/starter/db/connect.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectDB = (url) => {
-  return mongoose.connect(url);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Mongo DB connection established at ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Mongo DB connection failed due to: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
