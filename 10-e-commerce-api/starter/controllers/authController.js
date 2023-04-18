555c1ede77c6268c903bdb8e28cb360683813587
Vishesh Singh <VisheshSingh16@gmail.com>
2023-04-18 11:28:08 -0400

    validate if email exists

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -1,5 +1,15 @@
-const register = (req, res) => {
-  res.json({ msg: 'REGISTER' });
+const { BadRequestError } = require('../errors');
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+  const emailExists = await User.findOne({ email });
+  if (emailExists) {
+    throw new BadRequestError('Email already exists!');
+  }
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = (req, res) => {
