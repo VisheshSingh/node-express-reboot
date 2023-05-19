d6ec81f1e576b64f1416ec0cdde73a2abd86c321
Vishesh Singh <VisheshSingh16@gmail.com>
2023-05-18 21:12:14 -0400

    showMe controller

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -20,7 +20,9 @@ const getSingleUser = async (req, res) => {
 };
 
 const showCurrentUser = async (req, res) => {
-  res.send('Show Current User');
+  res.status(StatusCodes.OK).json({
+    user: req.user,
+  });
 };
 
 const updateUser = async (req, res) => {
--- 10-e-commerce-api/starter/routes/userRoutes.js
+++ 10-e-commerce-api/starter/routes/userRoutes.js
@@ -12,7 +12,7 @@ const auth = require('../middleware/authentication');
 const authorizeRoutes = require('../middleware/authorization');
 
 router.route('/').get(auth, authorizeRoutes('admin'), getAllUsers);
-router.route('/showMe').get(showCurrentUser);
+router.route('/showMe').get(auth, showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updatePassword').patch(updatePassword);
 router.route('/:id').get(getSingleUser);
