08e2116f0e0885fd11da8cce5979f3503b78ee23
Vishesh Singh <VisheshSingh16@gmail.com>
2023-05-20 09:16:30 -0400

    Product model

---
+++ 10-e-commerce-api/starter/models/Product.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a name for the product'],
+      maxlength: [100, 'Product name cannot exceed 100 characters'],
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add description for the product'],
+      maxlength: [1000, 'Please keep the description under 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/image.jpg',
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['office', 'kitchen', 'bedroom'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Please provide company'],
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+      default: ['#222'],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 10,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product;
