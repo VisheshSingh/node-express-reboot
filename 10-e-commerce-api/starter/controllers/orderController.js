c2dd22a11c92bee71201d3ff64fb8c73c0c8f16e
Vishesh Singh <VisheshSingh16@gmail.com>
2023-05-24 22:25:31 -0400

    order total

--- 10-e-commerce-api/starter/controllers/orderController.js
+++ 10-e-commerce-api/starter/controllers/orderController.js
@@ -1,9 +1,36 @@
 const { StatusCodes } = require('http-status-codes');
 const Order = require('../models/Order');
 const { NotFoundError, BadRequestError } = require('../errors');
+const Product = require('../models/Product');
 
 const createOrder = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'CREATE ORDER' });
+  const { items: cartItems, tax, shippingFee } = req.body;
+  if (!cartItems || cartItems.length < 1) {
+    throw new BadRequestError('No items in the cart!');
+  }
+  if (!tax || !shippingFee) {
+    throw new BadRequestError('No tax or shipping fee');
+  }
+  let orderItems = [];
+  let subtotal = 0;
+  for (let item of cartItems) {
+    const prodExists = await Product.findOne({ _id: item.product });
+    if (!prodExists) {
+      throw new NotFoundError('Product not found!');
+    }
+    const { name, price, image, _id } = prodExists;
+    const singleOrderItem = {
+      name,
+      price,
+      image,
+      amount: +item.amount,
+      product: _id,
+    };
+    orderItems = [...orderItems, singleOrderItem];
+    subtotal += +item.amount * price;
+    console.log({ orderItems, subtotal });
+  }
+  res.send('cr');
 };
 
 const getAllOrders = async (req, res) => {
