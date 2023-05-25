c167ae3b49beb060691cb37b9f219985ef32a5fa
Vishesh Singh <VisheshSingh16@gmail.com>
2023-05-24 20:26:27 -0400

    order schema

---
+++ 10-e-commerce-api/starter/models/Order.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SingleCartrItemSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: { type: mongoose.Types.ObjectId, ref: 'Product', required: true },
+});
+
+const OrderSchema = new Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    orderItems: {
+      type: [],
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'canceled', 'failed', 'paid', 'delivered'],
+      default: 'pending',
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model('Order', OrderSchema);
+
+module.exports = Order;
